d7747dd0f61ac38fbb1b270a8ab7afd2cb2051e8
Gareth8 <george.acklam.ga@gmail.com>
2025-04-05 17:36:57 +0100

    Initial Sql script for the database. Will probably end up being modified.

---
+++ CreateBookDb.sql
@@ -0,0 +1,28 @@
+CREATE TABLE authors (
+    author_ID INT PRIMARY KEY,
+    author_name varchar (255)
+);
+
+CREATE TABLE publishers (
+    publisher_ID INT PRIMARY KEY,
+    publisher_name varchar (255)
+);
+
+CREATE TABLE books (
+    book_ID INT PRIMARY KEY,
+    title varchar (100) not null,
+    author_ID INT,
+    publisher_ID INT,
+    genre varchar (30) not null,
+    ISBN varchar (13),
+    CONSTRAINT fk_author
+        FOREIGN KEY (author_ID)
+            REFERENCES authors(author_ID)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE,
+    CONSTRAINT fk_publisher
+        FOREIGN KEY (publisher_ID)
+            REFERENCES publishers(publisher_ID)
+                ON DELETE CASCADE
+                ON UPDATE CASCADE
+);
