c23fb8e373f053336de045cfb08da3fe0da4ff36
Gareth8 <george.acklam.ga@gmail.com>
2025-04-06 11:54:59 +0100

    Added a link table to connect books to authors, as this will allow for one book to have multiple authors. Also had the script check if the table exists or not.

--- CreateBookDb.sql
+++ CreateBookDb.sql
@@ -1,28 +1,33 @@
-CREATE TABLE authors (
+CREATE TABLE IF NOT EXISTS authors (
     author_ID SERIAL PRIMARY KEY,
     author_name varchar (60)
 );
 
-CREATE TABLE publishers (
+CREATE TABLE IF NOT EXISTS publishers (
     publisher_ID SERIAL PRIMARY KEY,
     publisher_name varchar (60)
 );
 
-CREATE TABLE books (
+CREATE TABLE IF NOT EXISTS books (
     book_ID SERIAL PRIMARY KEY,
     title varchar (100) not null,
-    author_ID INT,
     publisher_ID INT,
     genre varchar (30) not null,
     ISBN varchar (13),
-    CONSTRAINT fk_author
-        FOREIGN KEY (author_ID)
-            REFERENCES authors(author_ID)
-                ON DELETE CASCADE
-                ON UPDATE CASCADE,
     CONSTRAINT fk_publisher
         FOREIGN KEY (publisher_ID)
             REFERENCES publishers(publisher_ID)
                 ON DELETE CASCADE
                 ON UPDATE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS author_book_link (
+    book_ID INT,
+    author_ID INT,
+    CONSTRAINT fk_book
+        FOREIGN KEY (book_ID)
+            REFERENCES books(book_ID),
+    CONSTRAINT fk_author
+        FOREIGN KEY (author_ID)
+            REFERENCES authors(author_ID)
 );
